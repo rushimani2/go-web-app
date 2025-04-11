a99587fa2a95687c46a6696abcf1cb07cd8f76c0
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-04-12 01:00:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 )
 
@@ -15,7 +14,8 @@ func getPublicIP() (string, error) {
 	}
 	defer resp.Body.Close()
 
-	ip, err := ioutil.ReadAll(resp.Body)
+	// Use io.ReadAll instead of ioutil.ReadAll
+	ip, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
@@ -27,26 +27,8 @@ func getPublicIP() (string, error) {
 func ipPage(w http.ResponseWriter, r *http.Request) {
 	ip, err := getPublicIP()
 	if err != nil {
-		log.Printf("Error retrieving IP: %v", err)
 		http.Error(w, "Unable to retrieve IP address", http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprintf(w, "Your public IP address is: %s", ip)
-}
-
-// Handler to render a basic home page
-func homePage(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to the Home Page</h1>")
-}
-
-func main() {
-	http.HandleFunc("/", ipPage)
-	http.HandleFunc("/home", homePage)
-
-	log.Println("Server is running at http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
 }
