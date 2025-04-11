96e6e7e89bfbd3d62f7b36f46fad48de96c2b472
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-04-11 23:54:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -27,10 +27,18 @@ func getPublicIP() (string, error) {
 func ipPage(w http.ResponseWriter, r *http.Request) {
 	ip, err := getPublicIP()
 	if err != nil {
+		log.Printf("Error retrieving IP: %v", err)
 		http.Error(w, "Unable to retrieve IP address", http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprintf(w, "Your public IP address is: %s", ip)
 }
 
-
+func main() {
+	http.HandleFunc("/", ipPage)
+	log.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
