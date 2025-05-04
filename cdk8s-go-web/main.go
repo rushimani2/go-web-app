06b32875366a3861aa9ec2d43cd6e5aa97ebc310
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-04 23:27:59 +0530

    Update main.go

--- cdk8s-go-web/main.go
+++ cdk8s-go-web/main.go
@@ -1,56 +1,63 @@
-package main 
+package main
 
 import (
-    "cdk8s"
-    "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
-    "github.com/cdk8s-team/cdk8s-plus-go/cdk8splus25/v2"
-    "constructs"
+  "cdk8s"
+  "constructs"
+  "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
+  k8s "github.com/cdk8s-team/cdk8s-plus-go/cdk8splus25/v2"
 )
 
-type MyChart struct {
-    cdk8s.Chart
+type WebAppChart struct {
+  cdk8s.Chart
 }
 
-func NewMyChart(scope constructs.Construct, id string) cdk8s.Chart {
-    chart := cdk8s.NewChart(scope, &id)
+func NewWebAppChart(scope constructs.Construct, id string) cdk8s.Chart {
+  chart := cdk8s.NewChart(scope, &id)
 
-    // Deployment
-    deployment := cdk8splus25.NewDeployment(chart, jsii.String("WebAppDeployment"), &cdk8splus25.DeploymentProps{
-        Containers: &[]*cdk8splus25.ContainerProps{
-            {
-                Image: jsii.String("nginx:latest"),
-                Port:  jsii.Number(80),
-            },
+  label := map[string]*string{"app": cdk8s.String("go-web-app")}
+
+  deployment := k8s.NewDeployment(chart, cdk8s.String("go-web-deployment"), &k8s.DeploymentProps{
+    Metadata: &cdk8s.ApiObjectMetadata{
+      Name: cdk8s.String("go-web-deployment"),
+    },
+    Replicas: cdk8s.Float64(2),
+    Selector: &cdk8s.LabelSelector{
+      MatchLabels: &label,
+    },
+    PodMetadata: &cdk8s.ApiObjectMetadata{
+      Labels: &label,
+    },
+    Containers: &[]*k8s.Container{
+      {
+        Image: cdk8s.String("nginx:latest"),
+        Name:  cdk8s.String("nginx"),
+        Ports: &[]*k8s.ContainerPort{
+          {ContainerPort: cdk8s.Float64(80)},
         },
-    })
+      },
+    },
+  })
 
-    // Service
-    service := cdk8splus25.NewService(chart, jsii.String("WebAppService"), &cdk8splus25.ServiceProps{
-        Ports: &[]*cdk8splus25.ServicePort{
-            {
-                Port:       jsii.Number(80),
-                TargetPort: jsii.Number(80),
-            },
-        },
-        Metadata: &cdk8s.ApiObjectMetadata{
-            Labels: &map[string]*string{
-                "app": jsii.String("web-app"),
-            },
-        },
-    })
+  _ = deployment
 
-    service.AddSelectorLabel("app", "web-app")
-    service.ExposeDeployment(deployment)
+  k8s.NewService(chart, cdk8s.String("go-web-service"), &k8s.ServiceProps{
+    Metadata: &cdk8s.ApiObjectMetadata{
+      Name: cdk8s.String("go-web-service"),
+    },
+    Selector: &label,
+    Ports: &[]*k8s.ServicePort{
+      {
+        Port:       cdk8s.Float64(80),
+        TargetPort: cdk8s.Float64(80),
+      },
+    },
+  })
 
-    // Ingress
-    ingress := cdk8splus25.NewIngress(chart, jsii.String("WebAppIngress"), &cdk8splus25.IngressProps{})
-    ingress.AddHostDefaultBackend("/", deployment)
-
-    return chart
+  return chart
 }
 
 func main() {
-    app := cdk8s.NewApp(nil)
-    NewMyChart(app, "go-web-app")
-    app.Synth()
+  app := cdk8s.NewApp(nil)
+  NewWebAppChart(app, "go-web-app")
+  app.Synth()
 }
