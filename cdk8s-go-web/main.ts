25a7601e5d652f6684afdd79017d37f07950e2bb
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-06 16:56:09 +0530

    Update main.ts

--- cdk8s-go-web/main.ts
+++ cdk8s-go-web/main.ts
@@ -1,15 +1,33 @@
 import { App, Chart } from 'cdk8s';
 import { Construct } from 'constructs';
 import * as k8s from 'cdk8s-plus-25';
+
 class MyChart extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    new k8s.Deployment(this, 'web-deployment', {
+
+    const label = { app: 'web' };
+
+    const deployment = new k8s.Deployment(this, 'web-deployment', {
+      metadata: { name: 'web' }
+    });
+
+    deployment.addContainer({
+      image: 'nginx',
+      port: 80
+    });
+
+    deployment.select.addMatchLabel('app', 'web');
+
+    new k8s.Service(this, 'web-service', {
       metadata: { name: 'web' },
-      containers: [{ image: 'nginx' }]
+      type: k8s.ServiceType.CLUSTER_IP,
+      ports: [{ port: 80, targetPort: 80 }],
+      selector: deployment.select
     });
   }
 }
+
 const app = new App();
 new MyChart(app, 'my-chart');
 app.synth();
