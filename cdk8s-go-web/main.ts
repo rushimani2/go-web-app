dffae7a4e46c4ab11f4e830b430a713af2fa96ce
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-06 14:43:32 +0530

    Update main.ts

--- cdk8s-go-web/main.ts
+++ cdk8s-go-web/main.ts
@@ -6,9 +6,44 @@ class MyChart extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    // Create a Deployment
     new k8s.Deployment(this, 'web-deployment', {
       metadata: { name: 'web' },
-      containers: [{ image: 'nginx' }]
+      containers: [{ image: 'nginx' }],
+    });
+
+    // Create a Service
+    new k8s.Service(this, 'web-service', {
+      metadata: { name: 'web-service' },
+      spec: {
+        ports: [{ port: 80 }],
+        selector: { app: 'web' }, // match the Deployment
+      },
+    });
+
+    // Create an Ingress
+    new k8s.Ingress(this, 'web-ingress', {
+      metadata: { name: 'web-ingress' },
+      spec: {
+        rules: [
+          {
+            host: 'web.local', // You should change this for your real use case
+            http: {
+              paths: [
+                {
+                  path: '/',
+                  backend: {
+                    service: {
+                      name: 'web-service',
+                      port: { number: 80 },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
     });
   }
 }
